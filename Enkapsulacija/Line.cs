4d7d326c0d1933a774ee7562b3fed91f6d312ee0
Mislav Franjo <mislav.franja@gmail.com>
2020-10-26 18:22:23 +0100

    Resolve enkapsulation

---
+++ Enkapsulacija/Line.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enkapsulacija
+{
+    struct Point
+    {
+        public Point(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public readonly double X;
+        public readonly double Y;
+    }
+    class Line
+    {
+        public Line(double k, double l) {
+            a = -k;
+            c = -l;
+            b = 1;
+        }
+        public Line(Point p1, Point p2) {
+            a = p1.Y - p2.Y;
+            b = p2.X - p1.X;
+            c = p1.X * p2.Y - p2.X * p1.Y;
+        }
+
+        // ax + by + c=0;
+        public double Y(double x) { return -a/b * x - c/b; }
+
+        private double a;
+        private double b;
+        private double c;
+
+
+    }
+}
