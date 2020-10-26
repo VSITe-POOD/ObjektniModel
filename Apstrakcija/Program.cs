c090996cceb9b844eb34bdee05869ac1868829e0
Marin <angelo@MYPC>
2020-10-26 18:29:37 +0100

    resolve Apstraction and ecapsulation

--- Apstrakcija/Program.cs
+++ Apstrakcija/Program.cs
@@ -10,6 +10,22 @@ namespace Apstrakcija
     {
         static void Main(string[] args)
         {
+            line Line1 = new line(1, 0);
+            double x = 3;
+            Console.WriteLine($"y({x}) = {Line1.Y(x)}");
+
+            line Line2 = new line(2, 0);
+            Console.WriteLine($"y({x}) = {Line2.Y(x)}");
+
+            line Line3 = new line(2, -1);
+            Console.WriteLine($"y({x}) = {Line3.Y(x)}");
+
+            Point p1 = new Point(1, 2);
+            Point p2 = new Point(4, 7);
+            line Line4 = new line(p1,p2);
+            Console.Write($"y(4) = {Line4.Y(4)}");
+            Console.ReadKey();
+
         }
     }
 }
