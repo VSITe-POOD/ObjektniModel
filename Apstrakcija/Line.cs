ea550626c5284809cef114749fadec3976d5f32c
Mislav Franjo <mislav.franja@gmail.com>
2020-10-26 17:57:26 +0100

    resolve apstraction

---
+++ Apstrakcija/Line.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apstrakcija
+{
+    struct Point {
+        public Point(double x, double y) {
+            X = x;
+            Y = y;
+        }
+
+        public readonly double X;
+        public readonly double Y;
+    }
+
+    class Line
+    {
+        public Line(double k, double l) {
+            this.k = k;
+            this.l = l;
+        }
+
+        public Line(Point p1, Point p2) {
+            k = (p2.Y - p1.Y) / (p2.X - p1.X);
+            l = (p2.X * p1.Y - p1.X * p2.Y) / (p2.X - p1.X);
+        }
+
+        public double Y(double x) { return k * x + l; }
+
+        private double k;
+        private double l;
+    }
+}
