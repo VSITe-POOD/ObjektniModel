cbfcc5e0f1229663f0f539d0f090c24293dd92ae
Ante Mišković <ante.miskovic@adnet.hr>
2020-10-26 18:22:20 +0100

    Resolved encapsulation.

--- Enkapsulacija/Program.cs
+++ Enkapsulacija/Program.cs
@@ -10,6 +10,23 @@ namespace Enkapsulacija
     {
         static void Main(string[] args)
         {
-        }
+			Line line1 = new Line( 1, 0 );
+
+			double x = 3;
+			Console.WriteLine( $"y({x}) = {line1.Y( x )}" );
+
+			Line line2 = new Line( 2, 0 );
+			Console.WriteLine( $"y({x}) = {line2.Y( x )}" );
+
+			Line line3 = new Line( 2, -1 );
+			Console.WriteLine( $"y({x}) = {line3.Y( x )}" );
+
+			Point p1 = new Point( 1, 2 );
+			Point p2 = new Point( 4, 7 );
+			Line line4 = new Line( p1, p2 );
+			Console.WriteLine( $"y(4) = {line4.Y( 4 )}" );
+
+			Console.ReadKey();
+		}
     }
 }
