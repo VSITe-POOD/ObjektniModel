be672f5b005b4a2dec6002202bd00aeca9897885
Filip Velebit Samodol <fisamodo@vsite.hr>
2020-10-26 18:23:02 +0100

    Resolve encapsulation

--- Enkapsulacija/Line.cs
+++ Enkapsulacija/Line.cs
@@ -6,8 +6,30 @@ using System.Threading.Tasks;
 
 namespace Enkapsulacija
 {
+    struct Point
+    {
+        public Point(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+        public readonly double X;
+        public readonly double Y;
+    }
     class Line
     {
+        public Line(double k, double l)
+        {
+            a = -k;
+            c = -l;
+            b = 1;
+        }
+        public Line(Point p1, Point p2)
+        {
+            a = p1.Y - p2.Y;
+            b = p2.X - p1.X;
+            c = p1.X * p2.Y - p2.X * p1.Y;
+        }
         public double Y(double x)
         {
             return -a / b * x - c / b;
