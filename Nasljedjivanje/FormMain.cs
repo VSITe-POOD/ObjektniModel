e2b8e013d2564fd75e00428dd57af5b879e274ac
Marin <angelo@MYPC>
2020-10-26 19:24:14 +0100

    Inheritance

--- Nasljedjivanje/FormMain.cs
+++ Nasljedjivanje/FormMain.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
 
 namespace Nasljeđivanje
 {
@@ -15,6 +16,18 @@ namespace Nasljeđivanje
         public FormMain()
         {
             InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            TextBox tb = sender as TextBox;
+            if (tb != null)
+            {
+                if (tb.TextLength == 0)
+                {
+                    TextRenderer.DrawText(tb.CreateGraphics(), "Default value", tb.Font, tb.ClientRectangle, SystemColors.GrayText, TextFormatFlags.TextBoxControl);
+                }
+            }
         }
     }
 }
