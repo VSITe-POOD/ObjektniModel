d660dee6f2ea94991d1cb4d7688b5000612dac8c
Ante Miskovic <ante.miskovic@adnet.hr>
2020-11-16 18:51:06 +0100

    Implement polymorphism.

---
+++ Polimorfizam/Logging.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polimorfizam
+{
+    abstract class Logging
+    {
+        public abstract void Log(string text);
+
+    }
+
+    class ConsoleLogger : Logging
+    {
+        public override void Log(string text)
+        {
+            Console.WriteLine(text);
+        }
+    }
+
+    class FileLogger : Logging
+    {
+
+
+        StreamWriter writer = new StreamWriter("Log.txt");
+        public override void Log(string text)
+        {
+            writer.WriteLine(text);
+            writer.Flush();
+        }
+    }
+    class CompositeLogger : Logging
+    {
+        public void AddLogger(Logging log)
+        {
+            loggers.Add(log);
+        }
+        public override void Log(string text)
+        {
+            foreach (var logger in loggers)
+                logger.Log(text);
+        }
+        List<Logging> loggers = new List<Logging>();
+    }
+
+}
