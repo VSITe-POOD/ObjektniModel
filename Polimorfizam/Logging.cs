c6f6f8aea64d727f7eec5fd7e4a351498d8427b6
the.pero.forever@gmail.com <the.pero.forever@gmail.com>
2020-11-16 18:51:10 +0100

    Polimorfizam

---
+++ Polimorfizam/Logging.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polimorfizam
+{
+    abstract class Logging
+    {
+        public abstract void Log(String Text);
+    }
+    class ConsoleLogger : Logging
+    {
+        public override void Log(string Text)
+        {
+            Console.WriteLine(Text);
+        }
+    }
+    class FileLogger : Logging
+    {
+        public override void Log(string Text)
+        {
+            writer.WriteLine(Text);
+            writer.Flush();
+        }
+        StreamWriter writer = new StreamWriter("Log.txt");
+    }
+    class CompositeLogger : Logging
+    {
+        public override void Log(string Text)
+        {
+           foreach(var logger in loggers)
+            {
+                logger.Log(Text);
+            }
+        }
+        public void AddLogger(Logging log)
+        {
+            loggers.Add(log);
+        }
+        List<Logging> loggers = new List<Logging>();
+    }
+}
--- Polimorfizam/Program.cs
+++ Polimorfizam/Program.cs
@@ -10,6 +10,42 @@ namespace Polimorfizam
     {
         static void Main(string[] args)
         {
+            // logger = new ConsoleLogger();
+            // logger = new FileLogger();
+            logger = new CompositeLogger();
+            ((CompositeLogger)logger).AddLogger(new ConsoleLogger());
+            ((CompositeLogger)logger).AddLogger(new FileLogger());
+
+            int a = 5;
+            Method1(a);
+            a = -1;
+            Method1(a);
+            Console.ReadKey();
         }
+        static void Method1(int a)
+        {
+
+            logger.Log("I am entering Method 1");
+            if (a > 0)
+            {
+                Method2();
+            }
+            else
+            {
+                Method3();
+            }
+
+            logger.Log("I am leaving Method 1");
+        }
+        static void Method2()
+        {
+            logger.Log("I am in Method 2");
+        }
+        static void Method3()
+        {
+            logger.Log("I am in Method 3");
+        }
+
+        static Logging logger;
     }
 }
