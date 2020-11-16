f560d087415f18cf0daab0e09037115a0ddc17d6
Marin <angelo@MYPC>
2020-11-16 19:16:21 +0100

    Implement Polymorhism

---
+++ Polimorfizam/Logging.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polimorfizam
+{
+    abstract class Logging
+    {
+        public abstract void Log(string text);
+    }
+
+    class ConsoleLogger : Logging
+    {
+        public override void Log(string text)
+        {
+            Console.WriteLine(text);
+        }
+    }
+    class FileLogger : Logging
+    {
+        public override void Log(string text)
+        {
+            writer.WriteLine(text);
+            writer.Flush();
+        }
+
+        System.IO.StreamWriter writer = new System.IO.StreamWriter("Log.txt");
+    }
+
+    class CompositeLogger : Logging
+    {
+        public void AddLogger(Logging log)
+        {
+            loggers.Add(log);
+        }
+        public override void Log(string text)
+        {
+            foreach (var logger in loggers)
+                logger.Log(text);
+        }
+
+        List<Logging> loggers = new List<Logging>();
+    }
+}
