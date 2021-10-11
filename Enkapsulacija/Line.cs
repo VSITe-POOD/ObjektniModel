7e9f50b73e92c167f4d07feb2100f3ff18fb97b5
Marko Boroš <maboros@vsite.hr>
2021-10-11 18:43:22 +0200

    Add intersection logic for apstrakcija and enkapsulacija

--- Enkapsulacija/Line.cs
+++ Enkapsulacija/Line.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Apstrakcija
+namespace Enkapsulacija
 {
     interface ICurve
     {
@@ -32,34 +32,42 @@ namespace Apstrakcija
 
     class Line : ICurve,IIntersection
     {
+        public Line(double a,double b,double c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
         public Line(double slope,double yIntercept)
         {
-            this.slope = slope;
-            this.yIntercept = yIntercept;
+            a=- slope;
+            b = 1;
+            c=-yIntercept;
         }
         public double Y(double x)
         {
-            return slope * x + yIntercept;
+            return -(a * x) / b - (c / b);
+            //return slope * x + yIntercept;
         }
 
         public List<Point> GetIntersection(Line other)
         {
-            List<Point> result = new List<Point>();
-            if (slope == other.slope) 
-                return new List<Point>();
-            double nominator = other.yIntercept - yIntercept;
-            double denominator = slope - other.slope;
-            double x = nominator/denominator;
-            double y = slope*x+yIntercept;
-            return new List<Point> { new Point(x, y) };
+            //if (slope == other.slope) 
+            //    return new List<Point>();
+            double denominator = a * other.b - (other.a * b);
+            double x = b * other.c - other.b * c;
+            double y = c * other.a - other.c * a;
+            return new List<Point> { new Point(x/denominator, y/denominator) };
         }
 
         List<Point> IIntersection.GetIntersection(IIntersection other)
         {
             throw new NotImplementedException();
         }
-
-        private double slope;
-        private double yIntercept;
+        private double a;
+        private double b;
+        private double c;
+        //private double slope;
+        //private double yIntercept;
     }
 }
