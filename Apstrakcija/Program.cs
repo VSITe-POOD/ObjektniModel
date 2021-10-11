5f7adea15972fa5da57b1dc33c0ee478bdabca67
Marko Perica <maperica@vsite.hr>
2021-10-11 17:58:05 +0200

    Create ICurve interface and make Line to implement it

--- Apstrakcija/Program.cs
+++ Apstrakcija/Program.cs
@@ -10,6 +10,13 @@ namespace Apstrakcija
     {
         static void Main(string[] args)
         {
+            Line line = new Line(1, -1);
+
+            for (int x = -10; x < 10; ++x)
+                Console.WriteLine($"x={x}, y={line.Y(x)}");
+            
+
+            Console.ReadKey();
         }
     }
 }
