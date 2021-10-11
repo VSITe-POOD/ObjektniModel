4ee594b60181241c5e2964ee1e882afa438b4158
Paolo Stabile <pastabil@vsite.hr>
2021-10-11 18:15:40 +0200

    Add IIntersection and implementation

--- Apstrakcija/Line.cs
+++ Apstrakcija/Line.cs
@@ -10,7 +10,30 @@ namespace Apstrakcija
     {
         double Y(double x);
     }
-    class Line : ICurve
+
+    interface IIntersection
+    {
+        List<Point> GetIntersection(IIntersection other);
+    }
+
+    struct Point
+    {
+        public Point(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public override string ToString()
+        {
+            return $"({X}, {Y})";
+        }
+
+        public readonly double X;
+        public readonly double Y;
+    }
+
+    class Line : ICurve, IIntersection
     {
         public Line(double slope, double yInterception)
         {
@@ -21,6 +44,21 @@ namespace Apstrakcija
         {
             return slope * x + yInterception;
         }
+
+        public List<Point> GetIntersection(Line other)
+        {
+            if (slope == other.slope)
+                return new List<Point>();
+            double x = 0; // x kordinata sjecista
+            double y = 0; // y kordinata sjecista
+            return new List<Point> { new Point(x, y) };
+        }
+
+        List<Point> IIntersection.GetIntersection(IIntersection other)
+        {
+            throw new NotImplementedException();
+        }
+
         private double slope;
         private double yInterception;
     }
