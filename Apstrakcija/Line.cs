43cf16247c961bc316ceb3ec8ec9b0a9604b6dea
Ante Žunić <anzunic@vsite.hr>
2021-10-11 17:56:50 +0200

    Create ICurve interface and make line to implement it

--- Apstrakcija/Line.cs
+++ Apstrakcija/Line.cs
@@ -6,8 +6,23 @@ using System.Threading.Tasks;
 
 namespace Apstrakcija
 {
-    class Line
+    interface ICurve
     {
+        double Y(double x);
+    }
+    class Line : ICurve
+    {
+        public Line(double slope, double yInterception)
+        {
+            this.slope = slope;
+            yIntercept = yInterception;
+        }
+        public double Y(double x)
+        {
+            return slope * x + yIntercept;
+        }
 
+        private double slope;
+        private double yIntercept;
     }
 }
