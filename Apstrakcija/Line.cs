dbc71ebd6fa997f4a0704142e1d14d449941157b
Marko Boroš <maboros@vsite.hr>
2021-10-11 17:56:29 +0200

    Add ICurve interface and simple display of line

--- Apstrakcija/Line.cs
+++ Apstrakcija/Line.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace Apstrakcija
 {
-    class Line
+    interface ICurve
     {
+        double Y(double x);
+    }
+    class Line : ICurve
+    {
+        public Line(double slope,double yIntercept)
+        {
+            this.slope = slope;
+            this.yIntercept = yIntercept;
+        }
+        public double Y(double x)
+        {
+            return slope * x + yIntercept;
+        }
+
+        private double slope;
+        private double yIntercept;
     }
 }
