a2f35b6c5f4625f2414ba53fc40b50f13f3db852
Ana Horvatić <anhorvatic@vsite.hr>
2021-10-11 18:45:02 +0200

    Implement intersection calculation.

--- Enkapsulacija/Line.cs
+++ Enkapsulacija/Line.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Apstrakcija
+namespace Enkapsulacija
 {
     interface ICurve
     {
@@ -32,25 +32,34 @@ namespace Apstrakcija
     }
     class Line : ICurve, IIntersection
     {
+        public Line(double a, double b, double c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
         public Line(double slope, double yInterception)
         {
-            this.slope = slope;
-            this.yInterception = yInterception;
+            a = -slope;
+            b = 1;
+            c = -yInterception;
         }
         public double Y(double x)
         {
-            return slope * x + yInterception;
+            // return slope * x + yInterception;
+            return -a * x / b - c / b;
         }
 
         public List<Point> GetIntersection(Line other)
         {
-            if (slope == other.slope)
-                return new List<Point>();
-            double nominator = other.yInterception - yInterception;
-            double denominator = slope - other.slope;
-            double x = nominator / denominator; // x koordinata sjecista
-            double y = slope * x + yInterception; // y koordinata sjecista
-            return new List<Point> { new Point(x, y) };
+            // if (slope == other.slope)
+            //   return new List<Point>();
+            double denominator = a * other.b - other.a * b;
+            double x = b * other.c - other.b * c;
+            double y = c * other.a - other.c * a;
+            // double x = 0; // x koordinata sjecista
+          //   double y = 0; // y koordinata sjecista
+            return new List<Point> { new Point(x / denominator, y / denominator) };
         }
 
         List<Point> IIntersection.GetIntersection(IIntersection other)
@@ -58,7 +67,10 @@ namespace Apstrakcija
             throw new NotImplementedException();
         }
 
-        private double slope;
-        private double yInterception;
+        //private double slope;
+        //private double yInterception;
+        private double a;
+        private double b;
+        private double c;
     }
 }
