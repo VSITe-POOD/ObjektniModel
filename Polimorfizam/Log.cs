c0f19ffa67207d3009fa3268141e69e911ecfeb0
Ivan Bitunjac <ivbitunj@vsite.hr>
2021-10-18 18:33:17 +0200

    Implement polymorphic logger

---
+++ Polimorfizam/Log.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polimorfizam
+{
+    abstract class Log
+    {
+        public abstract void OutputString(string message);
+    }
+
+    class ConsoleLog : Log
+    {
+        public override void OutputString(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+    class FileLogger : Log
+    {
+        public FileLogger()
+        {
+            writer.AutoFlush = true;
+        }
+        public override void OutputString(string message)
+        {
+            writer.WriteLine(message);
+        }
+
+        StreamWriter writer = new StreamWriter("Log.txt");
+    }
+
+    class CompositeLogger : Log
+    {
+        List<Log>loggers = new List<Log>();
+
+        public CompositeLogger()
+        {
+            loggers.Add(new ConsoleLog());
+            loggers.Add(new FileLogger());
+        }
+
+        public void AddLogger(Log logger)
+        {
+            loggers.Add(logger);
+        }
+
+        public override void OutputString(string message)
+        {
+            foreach(Log logger in loggers)
+            {
+                logger.OutputString(message);
+            }   
+        }
+    }
+}
--- Polimorfizam/Program.cs
+++ Polimorfizam/Program.cs
@@ -8,8 +8,50 @@ namespace Polimorfizam
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
+            Console.WriteLine("U datoteku (D), na ekran (E) ili oboje (O)?");
+            var outputStream = Console.ReadKey();
+            Console.WriteLine("\n");
+            switch (outputStream.KeyChar)
+            {
+                case 'd':
+                    logger = new FileLogger();
+                    break;
+                case 'e':
+                    logger = new ConsoleLog();
+                    break;
+                case 'o':
+                    logger = new CompositeLogger();
+                    break;
+                default:
+                    break;
+            }
+            logger.OutputString("Start in Main()");
+            metoda1(1);
+            metoda1(16);
+            logger.OutputString("End in Main()");
+            Console.ReadKey();
         }
+
+        private static void metoda1(int a)
+        {
+            logger.OutputString("Start in metoda1()");
+            if (a % 2 == 0) metodaParna();
+            else metodaNeparna();
+        }
+
+        private static void metodaParna()
+        {
+            logger.OutputString("Start in metodaParna");
+        }
+
+        private static void metodaNeparna()
+        {
+            logger.OutputString("Start in metodaNeparna");
+        }
+
+        static Log logger = new ConsoleLog();
     }
 }
