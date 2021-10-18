712eda264e46d46d3354d6735237dd523e2fafec
Marko Boroš <maboros@vsite.hr>
2021-10-18 18:05:49 +0200

    Initial implementation of logger

---
+++ Polimorfizam/Log.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polimorfizam
+{
+    class Log
+    {
+        public void Output(String message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
--- Polimorfizam/Program.cs
+++ Polimorfizam/Program.cs
@@ -8,8 +8,32 @@ namespace Polimorfizam
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
+            logger.Output("Start in main");
+            Metoda1(1);
+            Metoda1(2);
+            logger.Output("End in main");
         }
+        private static void Metoda1(int a)
+        {
+            logger.Output("Start in Metoda1");
+            if (a % 2 == 0)
+            {
+                MetodaParna();
+                return;
+            }
+            MetodaNeparna();
+        }
+        private static void MetodaParna()
+        {
+            logger.Output("Start in Parna");
+        }
+        private static void MetodaNeparna()
+        {
+            logger.Output("Start in Neparna");
+        }
+        static Log logger = new Log();
     }
 }
