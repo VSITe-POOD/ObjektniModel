9ef856b8082408fbb371255890aef7ebd8c29709
Ante Žunić <anzunic@vsite.hr>
2021-10-18 18:05:55 +0200

    Initial implementation of logger

--- Polimorfizam/Program.cs
+++ Polimorfizam/Program.cs
@@ -10,6 +10,31 @@ namespace Polimorfizam
     {
         static void Main(string[] args)
         {
+            logger.Output("Start in main");
+            metoda1(1);
+            metoda1(4);
+            logger.Output("End in main");
         }
+
+        private static void metoda1(int a)
+        {
+            logger.Output("Start in metoda1");
+            if (a % 2 != 0)
+                metodaNeparna();
+            else
+                metodaParna();
+        }
+
+        private static void metodaParna()
+        {
+            logger.Output("Start in metodaParna");
+        }
+
+        private static void metodaNeparna()
+        {
+            logger.Output("Start in metodaNeparna");
+        }
+
+        static Log logger = new Log();
     }
 }
