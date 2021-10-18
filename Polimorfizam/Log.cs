4f1f02358b0fd633e9e10d15209eab28165ceb71
klarakoscevic <klkoscevic@vsite.hr>
2021-10-18 18:44:33 +0200

    Implement logger inheritance

--- Polimorfizam/Log.cs
+++ Polimorfizam/Log.cs
@@ -1,13 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Polimorfizam {
-    class Log {
-        public void Output(string message) {
+    abstract class Log {
+        public abstract void Output(string message);
+    }
+
+    class ConsoleLog : Log {
+        public override void Output(string message) {
             Console.WriteLine(message);
         }
+    }
+
+    class FileLog : Log {
+
+        public FileLog() {
+            writer.AutoFlush = true;
+        }
+        public override void Output(string message) {
+            writer.WriteLine(message);
+        }
+
+        StreamWriter writer = new StreamWriter("Log.txt");
+    }
+
+    class CompositeLog : Log {
+        public CompositeLog() {
+            loggers.Add(new FileLog());
+            loggers.Add(new ConsoleLog());
+        }
+        public void AddLogger(Log logger) {
+            loggers.Add(logger);
+        }
+        public override void Output(string message) {
+            foreach(var logger in loggers) {
+                logger.Output(message);
+            }
+        }
+
+        List<Log> loggers = new List<Log>();
     }
 }
