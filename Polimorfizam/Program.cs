fabcd227c250469b7d19ab961b1f89de3d31329e
klarakoscevic <klkoscevic@vsite.hr>
2021-10-18 18:06:00 +0200

    Initial implement of logger class

--- Polimorfizam/Program.cs
+++ Polimorfizam/Program.cs
@@ -10,6 +10,30 @@ namespace Polimorfizam
     {
         static void Main(string[] args)
         {
+            logger.Output("Start in Main");
+            Metoda1(1);
+            Metoda1(4);
+            logger.Output("End in Main");
         }
+
+        private static void Metoda1(int a) {
+            logger.Output("Start in Metoda1");
+
+            if (a % 2 != 0)
+                MetodaNeparna();
+            else
+                MetodaParna();
+        }
+
+        private static void MetodaParna() {
+            logger.Output("Start in MetodaParna");
+        }
+
+        private static void MetodaNeparna() {
+            logger.Output("Start in MetodaNeparna");
+
+        }
+
+        static Log logger = new Log();
     }
 }
