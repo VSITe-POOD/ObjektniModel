625d7b98ab457e24505cc2c078890b40e74c79a5
Antonio Brkic <ABrkic@dspace.hr>
2022-02-28 20:23:54 +0100

    Enkapsulate line operations into class

---
+++ Apstrakcija/Line.cs
@@ -0,0 +1,36 @@
+﻿namespace Apstrakcija
+{
+    struct Point
+    {
+        public Point(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public readonly double X;
+        public readonly double Y;
+    }
+    class Line
+    {
+        public Line(double k, double l)
+        {
+            K = k;
+            L = l;
+        }
+
+        public Point? Intersection(Line other)
+        {
+            if (K == other.K)
+            {
+                return null;
+            }
+            double x = (other.L - L) / (K - other.K);
+            double y = K * x + L;
+            return new Point(x, y);
+        }
+
+        public readonly double K;
+        public readonly double L;
+    }
+}
--- Apstrakcija/Program.cs
+++ Apstrakcija/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Apstrakcija
 {
@@ -10,25 +6,36 @@ namespace Apstrakcija
     {
         static void Main(string[] args)
         {
-            try
-            {
-                double k1 = double.Parse(args[0]);
-                double l1 = double.Parse(args[1]);
-                double k2 = double.Parse(args[2]);
-                double l2 = double.Parse(args[3]);
+            //try
+            //{
+            //    double k1 = double.Parse(args[0]);
+            //    double l1 = double.Parse(args[1]);
+            //    double k2 = double.Parse(args[2]);
+            //    double l2 = double.Parse(args[3]);
 
-                if (k1 == k2)
-                    Console.WriteLine("Lines are parallel");
-                else
-                {
-                    double x = (l2 - l1) / (k1 - k2);
-                    double y = k1 * x + l1;
-                    Console.WriteLine($"Intersection: ({x}, {y})");
-                }
-            }
-            catch (Exception)
+            //    if (k1 == k2)
+            //        Console.WriteLine("Lines are parallel");
+            //    else
+            //    {
+            //        double x = (l2 - l1) / (k1 - k2);
+            //        double y = k1 * x + l1;
+            //        Console.WriteLine($"Intersection: ({x}, {y})");
+            //    }
+            //}
+            //catch (Exception)
+            //{
+            //    Console.WriteLine("Invalid entry");
+            //}
+            Line l1 = new Line(1, 0);
+            Line l2 = new Line(0, 1);
+            Point? p = l1.Intersection(l2);
+            if (p.HasValue)
             {
-                Console.WriteLine("Invalid entry");
+                Console.WriteLine($"Intersection: ({p.Value.X}, {p.Value.Y})");
+            }
+            else
+            {
+                Console.WriteLine("Lines are parallel");
             }
         }
     }
