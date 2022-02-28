ef0f7d60261c219045e468eb9893bed37a6d6c9b
Damir Stojanović <DStojanovic@dspace.hr>
2022-02-28 20:39:28 +0100

    Resolve Intersection

---
+++ Apstrakcija/line.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apstrakcija
+{
+    public struct Point
+    {
+
+        public Point(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public readonly double X;
+        public readonly double Y;
+
+    }
+    public class Line
+    {
+        public Line(double k, double l)
+        {
+            this.k = k;
+            this.l = l;
+        }
+        public Point? Intersection(Line other)
+        {
+            if (k == other.k)
+                return null;
+            
+            double x = (other.l - l) / (k - other.k);
+            double y = k * x + l;
+            return new Point(x, y);
+        }
+
+        public readonly double k;
+        public readonly double l;
+
+    }
+}
