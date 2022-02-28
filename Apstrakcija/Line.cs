ed57a886cd046b9e1f17a42a02331f62ba090043
Andrea Vrljic <AVrljic@dspace.hr>
2022-02-28 20:23:59 +0100

    Encapsulate Line operations into a class

---
+++ Apstrakcija/Line.cs
@@ -0,0 +1,39 @@
+﻿namespace Apstrakcija
+{
+    struct Point
+    {
+        public readonly double X;
+        public readonly double Y;
+
+        public Point(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+    }
+    class Line
+    {
+        public readonly double k;
+        public readonly double l;
+
+        public Line(double k, double l)
+        {
+            this.k = k;
+            this.l = l;
+
+        }
+        public Point? Intersection(Line other)
+        {
+            if (k == other.k)
+            {
+                return null;
+            }
+
+            double x = (other.l - l) / (k - other.k);
+            double y = k + x + l;
+            return new Point(x, y);
+
+        }
+    }
+}
