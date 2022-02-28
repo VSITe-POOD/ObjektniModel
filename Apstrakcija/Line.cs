ef56058eb2e21a6f4cdca2b053d922f10556f2d3
AndjelaJ <AJuric@dspace.hr>
2022-02-28 20:23:55 +0100

    Encapsulate Line operations into a class.

---
+++ Apstrakcija/Line.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apstrakcija
+{
+    struct Point
+    {
+
+        public Point(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public readonly double X;
+        public readonly double Y;
+    }
+    class Line
+    {
+        public Line(double k, double l)
+        {
+            K = k;
+            L = l;
+        }
+
+        public Point? Intersection(Line other)
+        {
+            if (K == other.K)
+                return null;
+
+            double x = (other.L - L) / (K - other.K);
+            double y = K * x + L;
+
+            return new Point(x, y);
+        }
+
+        public readonly double K;
+        public readonly double L;
+    }
+}
