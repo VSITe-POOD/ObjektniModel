01f57fb4f345ed2acbb37a3b32fae8afb4746208
Vedran Olujic <vedranolujic23@gmail.com>
2022-02-28 20:26:00 +0100

    lab1

--- Apstrakcija/Program.cs
+++ Apstrakcija/Program.cs
@@ -10,6 +10,7 @@ namespace Apstrakcija
     {
         static void Main(string[] args)
         {
+            /*
             try
             {
                 double k1 = double.Parse(args[0]);
@@ -30,6 +31,14 @@ namespace Apstrakcija
             {
                 Console.WriteLine("Invalid entry");
             }
+            */
+            Line l1 = new Line(1, 0);
+            Line l2 = new Line(0, 1);
+            Point? p = l1.Intersection(l2);
+            if (p.HasValue)
+                Console.WriteLine("Has value");
+            else
+                Console.WriteLine("It doesnt!");
         }
     }
 }
