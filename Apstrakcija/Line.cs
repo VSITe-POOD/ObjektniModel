34fa284d097e0d6066ff6a46c0fa7387e8c6dff7
Ida Midžić <idmidzic@vsite.hr>
2022-02-28 20:39:44 +0100

    Commit adding Line.cs

---
+++ Apstrakcija/Line.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apstrakcija
+
+struct Point
+{
+	public Point(double x, double y)
+	{
+		X = x;
+		Y = y;	
+	}
+	public readonly double X;
+	public readonly double Y;
+}
+
+class Line
+{
+	public Line(double k, double l)
+	{
+		this.k = k;
+		this.l = l;
+	}
+
+	public Point? Intersection(Line other)
+    {
+
+    }
+	public readonly double k;
+	public readonly double l;
+}
--- Apstrakcija/Program.cs
+++ Apstrakcija/Program.cs
@@ -30,6 +30,7 @@ namespace Apstrakcija
             {
                 Console.WriteLine("Invalid entry");
             }
+
         }
     }
 }
