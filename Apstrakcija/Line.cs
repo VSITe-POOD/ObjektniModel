7857e0cbb3773fae347a1eba3c144b82759f580d
Ida Midžić <idmidzic@vsite.hr>
2022-03-06 19:47:54 +0100

    Final check in, Line cs added

--- Apstrakcija/Line.cs
+++ Apstrakcija/Line.cs
@@ -5,30 +5,38 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Apstrakcija
-
-struct Point
 {
-	public Point(double x, double y)
+	struct Point
 	{
-		X = x;
-		Y = y;	
-	}
-	public readonly double X;
-	public readonly double Y;
-}
-
-class Line
-{
-	public Line(double k, double l)
-	{
-		this.k = k;
-		this.l = l;
+		public Point(double x, double y)
+		{
+			X = x;
+			Y = y;
+		}
+		public readonly double X;
+		public readonly double Y;
 	}
 
-	public Point? Intersection(Line other)
-    {
+	class Line
+	{
+		public Line(double k, double l)
+		{
+			this.k = k;
+			this.l = l;
+		}
 
-    }
-	public readonly double k;
-	public readonly double l;
+		public Point? Intersection(Line other)
+		{
+			if (k == other.k)
+				return null;
+
+			double x = (other.l - l) / (k - other.k);
+			double y = k * x + 1;
+			return new Point(x, y);
+		}
+
+		public readonly double k;
+		public readonly double l;
+	}
 }
+
