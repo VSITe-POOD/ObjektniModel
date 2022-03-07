a80d2aed1eb5a6361eb310c532eb39c2ec87b540
Mato Kocelj <MatoK@dspace.hr>
2022-03-07 19:55:11 +0100

    -add new text box with text box assigned

--- Nasljedjivanje/FormMain.Designer.cs
+++ Nasljedjivanje/FormMain.Designer.cs
@@ -28,16 +28,10 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBoxDefault1 = new Nasljeđivanje.TextBoxDefault();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBoxDefault1 = new Nasljeđivanje.TextBoxDefault();
+            this.textBox2 = new Nasljeđivanje.TextBoxDefault();
             this.SuspendLayout();
-            // 
-            // textBoxDefault1
-            // 
-            this.textBoxDefault1.Location = new System.Drawing.Point(32, 59);
-            this.textBoxDefault1.Name = "textBoxDefault1";
-            this.textBoxDefault1.Size = new System.Drawing.Size(100, 22);
-            this.textBoxDefault1.TabIndex = 0;
             // 
             // textBox1
             // 
@@ -46,11 +40,26 @@
             this.textBox1.Size = new System.Drawing.Size(100, 22);
             this.textBox1.TabIndex = 1;
             // 
+            // textBoxDefault1
+            // 
+            this.textBoxDefault1.Location = new System.Drawing.Point(32, 59);
+            this.textBoxDefault1.Name = "textBoxDefault1";
+            this.textBoxDefault1.Size = new System.Drawing.Size(100, 22);
+            this.textBoxDefault1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(32, 115);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(210, 22);
+            this.textBox2.TabIndex = 2;
+            // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.textBoxDefault1);
             this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
@@ -65,6 +74,7 @@
 
         private TextBoxDefault textBoxDefault1;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
