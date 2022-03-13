0d12ed436221b9fd7fef7da4128b4826c82861b0
Damir Stojanović <DStojanovic@dspace.hr>
2022-03-13 11:14:29 +0100

    Changed textBox2 to TextBoxDefault

--- Nasljedjivanje/FormMain.Designer.cs
+++ Nasljedjivanje/FormMain.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBoxDefault1 = new Nasljeđivanje.TextBoxDefault();
+            this.textBox2 = new Nasljeđivanje.TextBoxDefault();
             this.SuspendLayout();
             // 
             // textBox1
@@ -46,11 +47,19 @@
             this.textBoxDefault1.Size = new System.Drawing.Size(188, 22);
             this.textBoxDefault1.TabIndex = 1;
             // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(34, 124);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(269, 22);
+            this.textBox2.TabIndex = 2;
+            // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBoxDefault1);
             this.Controls.Add(this.textBox1);
             this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
@@ -65,6 +74,7 @@
 
         private System.Windows.Forms.TextBox textBox1;
         private TextBoxDefault textBoxDefault1;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
