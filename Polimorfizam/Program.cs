54fa6211697562d97b011c84946e6371d4eef48e
Luka Gregorić <endo145@gmail.com>
2022-03-21 18:40:15 +0100

    Implement polymorphic story writing

--- Polimorfizam/Program.cs
+++ Polimorfizam/Program.cs
@@ -12,7 +12,10 @@ namespace Polimorfizam
         {
             Console.OutputEncoding = Encoding.UTF8;
 
-            StoryTeller st = new StoryTeller();
+            //var writer = new PlainTextWriter();
+            var writer = new HtmlWriter();
+            //DZ var writer = new JsonWriter();
+            StoryTeller st = new StoryTeller(writer);
             st.WriteHeading1("Priče iz davnine");
             st.WriteHeading2("Šuma Striborova");
             st.WriteParagraph("Zašao neki momak u šumu Striborovu, a nije znao da je ono šuma začarana i da se u njoj svakojaka čuda zbivaju.");
