038fe46eb60524d0a7d36983db043bc1e9daa97e
Vicko Cagalj <vickocagalj@gmail.com>
2022-03-27 18:48:34 +0200

    Implement JsonWriter DZ

---
+++ Polimorfizam/JsonWriter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polimorfizam 
+{
+    class JsonWriter : IFormatWriter
+    {
+        public void WriteHeading1(string caption)
+        {
+            Console.WriteLine("{");
+            Console.WriteLine($"\t\"h1\": \"{caption}\"");
+        }
+
+        public void WriteHeading2(string caption)
+        {
+            Console.WriteLine($"\t\"h2\": \"{caption}\"");
+        }
+
+        public void WriteParagraph(string text)
+        {
+            Console.WriteLine($"\t\"p\": \"{text}\"");
+            Console.WriteLine("}");
+        }
+    }
+}
